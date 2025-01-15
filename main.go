5f6d51db4d153c990ffd4a58f6c33707dab6268e
course <zhizntumannaya@gmail.com>
2025-01-15 03:53:13 +0300

    Still easy, but it's starting to get more interesting.

--- main.go
+++ main.go
@@ -2,16 +2,16 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
 
-	var originalCount int = 10
-	fmt.Println("I started with", originalCount)
-	var eatenCount = originalCount - 6
-	fmt.Println("Some jerk ate", eatenCount, "apples")
-	eatenCount = eatenCount + 2
-
-	fmt.Println("There are ", eatenCount, "apples left")
+	now := time.Now().Weekday()
+	year := time.Now().Year()
+	fmt.Println(now,"\n",year)
+	
+	
+	
 
 }
