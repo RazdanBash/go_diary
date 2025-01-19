a88a4ab5ae876d011dec75382f26c59e10f5a454
y.tumanyan <y.tumanyan@mkskom.ru>
2025-01-19 20:17:51 +0300

    found out that this book is old version

--- main.go
+++ main.go
@@ -1,15 +1,39 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
+	"math/rand"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	fileInfo, err := os.Stat("my.txt")
+	target := rand.Intn(100) + 1
+	fmt.Println(target)
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Make a guess:")
+	input, err := reader.ReadString('\n')
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(fileInfo.Size())
+	input = strings.TrimSpace(input)
+	guess, err := strconv.Atoi(input)
+	if err != nil {
+		log.Fatal(err)
+
+		for guess := 10; guess == 0; guess-- {
+		}
+		if guess < target {
+			fmt.Println("Меньше загаданного числа")
+		}
+		if guess > target {
+			fmt.Println("Больше загаданного числа")
+		}
+		if guess == target {
+			fmt.Println("Вы угадали !")
+		}
+	}
 }
