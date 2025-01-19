7b5e08752beae285a4d030d68dd61b138d5c4608
course <zhizntumannaya@gmail.com>
2025-01-19 23:22:59 +0300

    afk day

--- main.go
+++ main.go
@@ -1,39 +1,14 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"log"
-	"math/rand"
-	"os"
-	"strconv"
-	"strings"
+	
 )
 
 func main() {
-	target := rand.Intn(100) + 1
-	fmt.Println(target)
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Make a guess:")
-	input, err := reader.ReadString('\n')
-	if err != nil {
-		log.Fatal(err)
-	}
-	input = strings.TrimSpace(input)
-	guess, err := strconv.Atoi(input)
-	if err != nil {
-		log.Fatal(err)
-
-		for guess := 10; guess == 0; guess-- {
-		}
-		if guess < target {
-			fmt.Println("Меньше загаданного числа")
-		}
-		if guess > target {
-			fmt.Println("Больше загаданного числа")
-		}
-		if guess == target {
-			fmt.Println("Вы угадали !")
-		}
-	}
+	var width, height, area float64
+width = 4.2
+height = 3.0
+area = width * height
+fmt.Println(area/10.0, "liters needed")
 }
